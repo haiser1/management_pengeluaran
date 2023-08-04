824172773906e31fe8ff7529dcd9c5b68c10fcd6
Mark <haiser109@gmail.com>
2023-08-04 23:51:22 +0700

    update .gitingnore

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,10 @@
 import { Sequelize } from "sequelize"
 
-const db = new Sequelize('pengeluaran', 'root', 'password', {
+
+const db = new Sequelize(process.env.DB_NAME, 'root', process.env.DB_PASSWORD, {
     host: 'localhost',
     dialect: 'mysql'
 })
+console.log(process.env.DB_USERNAME)
 
 export default db
